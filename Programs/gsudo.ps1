93db15aae59537d0bb244d7b7551a052d6301447
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-14 15:14:28 -0800

    Simplified gsudo

--- Programs/gsudo.ps1
+++ Programs/gsudo.ps1
@@ -1,5 +1,2 @@
-#Requires -RunAsAdministrator
-# Installation fails when not run as admin for some reason
-
 # --location does not work
 winget install gerardog.gsudo --override "InstallFolder=C:\Programs\gsudo /quiet"
