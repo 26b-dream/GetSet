e5cb4d04d9645d2057fe9234b1301097c9354b14
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-14 15:14:18 -0800

    Simplified Everything

--- Programs/Everything.ps1
+++ Programs/Everything.ps1
@@ -1,12 +1,8 @@
 # --location does not work
-# winget install voidtools.Everything --override "INSTALLDIR=C:\Programs\Everything /quiet" -i
+winget install voidtools.Everything --override "INSTALLDIR=C:\Programs\Everything /quiet"
 
-#Requires -RunAsAdministrator
-# Shortcut cannot be removed unless run as administrator
-$shortcut = "C:\Users\Public\Desktop\Everything.lnk"
-if (Test-Path $shortcut) {
-    Remove-Item -Path $shortcut -Force
-}
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\delete_shortcut.ps1"
+delete_shortcut "Everything"
 
 # Everything does not start automatically after installation so it is safe to restore config after installation
 Copy-Item "files\Everything\Everything.ini" "C:\Programs\Everything\Everything.ini"
