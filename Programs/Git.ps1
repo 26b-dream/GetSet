7e22020f2e74c9f1f49b713cd8f8126712c5bbff
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:12:30 -0800

    Improved comment

--- Programs/Git.ps1
+++ Programs/Git.ps1
@@ -1,4 +1,4 @@
-# For some reason /EditorOption=VisualStudioCode doesn't work using the command line, but it works when loading from the ini file
-#   Example: winget install Git.Git --location C:\Programs\Git --override "/Dir=C:\Programs\Git /EditorOption=VisualStudioCode /Components=!ext\shellhere,!ext\guihere,ext,ext,gitlfs,assoc,assoc_sh" -i
+# Does not set default editor correctly
+#   winget install Git.Git --location C:\Programs\Git --override "/Dir=C:\Programs\Git /EditorOption=VisualStudioCode /Components=!ext\shellhere,!ext\guihere,ext,ext,gitlfs,assoc,assoc_sh"
 
 winget install Git.Git --location C:\Programs\Git --override "/LOADINF=files\Git\install.inf /SILENT" -i
