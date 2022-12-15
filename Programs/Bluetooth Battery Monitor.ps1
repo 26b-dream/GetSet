4d00ce1cb87a398b8806ecaa73b8dc26f25089ea
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:11:06 -0800

    Improved comments

--- Programs/Bluetooth Battery Monitor.ps1
+++ Programs/Bluetooth Battery Monitor.ps1
@@ -1,7 +1,5 @@
-# Not available on Winget
-# Not available on Chocolatey
-# Need to download and install manually
-
+# Not available via winget
+# Not available via choco
 $name = "Bluetooth Battery Monitor"
 $version = "2.20.0.1"
 $download_folder = "$env:USERPROFILE\Downloads\get_set\$name"
@@ -12,5 +10,6 @@ $exe_path = "$download_folder\BattMonSetup-$version.msi"
 . "$((get-item $PSScriptRoot).parent.FullName)\lib\download.ps1"
 download $url $exe_path
 
-# Normal GUI installation has no choice on installation path
+# Installer has no method to modify install path
+# TODO: Maybe use Start-Process on top of this
 msiexec /i "$exe_path" /qn /quiet
