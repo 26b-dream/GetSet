054c7531b94ac0fab9220e77f6699299d5fbc00d
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:11:36 -0800

    Improved comment

--- Programs/Discord.ps1
+++ Programs/Discord.ps1
@@ -1,4 +1,4 @@
-# Impossible to modify installation path using installer
+# Installer has no method to modify install path
 winget install Discord.Discord --location C:\Programs\Discord
 
 . "$((get-item $PSScriptRoot).parent.FullName)\lib\delete_shortcut.ps1"
