2553524c2ab7d13b774631a638d56d199c8c3444
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:10:18 -0800

    Added shortcut creation function

---
+++ lib/create_shortcut.ps1
@@ -0,0 +1,6 @@
+function create_shortcut([string]$SourceExe, [string]$DestinationPath) {
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($DestinationPath)
+    $Shortcut.TargetPath = $SourceExe
+    $Shortcut.Save()
+}
