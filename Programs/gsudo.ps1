8a68bb8d2c9c54635e4575700eb890d0b86b19df
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:12:36 -0800

    Improved comment

--- Programs/gsudo.ps1
+++ Programs/gsudo.ps1
@@ -1,2 +1,3 @@
-# --location does not work
+# Installs into the wrong path
+#   winget install gerardog.gsudo --location "C:\Programs\gsudo"
 winget install gerardog.gsudo --override "InstallFolder=C:\Programs\gsudo /quiet"
