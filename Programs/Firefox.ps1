eb58d6b597476e52b8f11056bbd58f7d6e071031
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:11:56 -0800

    Improved comment

--- Programs/Firefox.ps1
+++ Programs/Firefox.ps1
@@ -1,2 +1,3 @@
-# --location does not work
+# Installs into the wrong path
+#   winget install Mozilla.Firefox --location "C:\Programs\Firefox"
 winget install Mozilla.Firefox --override "/InstallDirectoryPath=C:\Programs\Firefox /TaskbarShortcut=False /DesktopShortcut=False"
