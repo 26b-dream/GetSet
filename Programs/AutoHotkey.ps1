8abd09333070305ea31bdc8fbca3b757387d9db5
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 00:10:47 -0800

    Improved comment

--- Programs/AutoHotkey.ps1
+++ Programs/AutoHotkey.ps1
@@ -1,2 +1,3 @@
-# --location does not work
+# Installs into the wrong path
+#   winget install Lexikos.AutoHotkey --location "C:\Programs\AutoHotkey"
 winget install Lexikos.AutoHotkey --override "/D=C:\Programs\AutoHotkey /S"
