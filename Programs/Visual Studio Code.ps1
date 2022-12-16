0d26e460ff7a9d783b280f5454f029b0027adf31
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 17:01:09 -0800

    Removed mistake

--- Programs/Visual Studio Code.ps1
+++ Programs/Visual Studio Code.ps1
@@ -1,3 +1,2 @@
-winget install --id Microsoft.VisualStudioCode --silent --location "C:\Programs\Visual Studio Code"
-
+# TODO: Explain this
 winget install --id Microsoft.VisualStudioCode --override '/MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders /DIR="C:\Programs\Visual Studio Code"'
