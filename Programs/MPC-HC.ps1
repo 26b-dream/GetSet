1679e9f24bb6a0d3c2c003350c75f1137bcf3900
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-15 16:18:03 -0800

    Added MPC-HC

--- Programs/MPC-HC.ps1
+++ Programs/MPC-HC.ps1
@@ -1,1 +1,5 @@
 winget install clsid2.mpc-hc --location "C:\Programs\MPC-HC"
+
+# Because the installer is a little bit broken the shortcut will be caleld False instead of actually not created
+. "$((get-item $PSScriptRoot).parent.FullName)\lib\delete_shortcut.ps1"
+delete_shortcut "MPC-HC x64"
