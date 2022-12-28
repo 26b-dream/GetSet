27f66034f86488b8d0b0a6b5bcd6e06fd0109a15
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-12-27 22:55:05 -0800

    Added Acrobat Reader

---
+++ Programs/Adobe Acrobat Reader.ps1
@@ -0,0 +1,4 @@
+# Installer is all kinds of busted, won't even run normally
+# Don't bother trying to change install path
+winget install Adobe.Acrobat.Reader.64-bit --location "C:\Programs\Adobe Acrobat Reader
+"
