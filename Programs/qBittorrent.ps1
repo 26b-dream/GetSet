061754f398f7fdb0601a6c1d3e6c2673397524e4
26b-dream <84663588+26b-dream@users.noreply.github.com>
2023-01-01 01:57:27 -0800

    Added qBittorrent

---
+++ Programs/qBittorrent.ps1
@@ -0,0 +1,5 @@
+# Installer has no method to modify install path
+winget install qBittorrent.qBittorrent --location C:\Programs\qBittorrent
+
+# . "$((get-item $PSScriptRoot).parent.FullName)\lib\delete_shortcut.ps1"
+# delete_shortcut "qBittorrent"
